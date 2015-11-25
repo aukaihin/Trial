1ce1a7412ec9568691a8cd076266a6f29c50e287
AU KAI HIN <aukaihin@gmail.com>
2015-11-25 11:58:24 +0800

    new_project

---
+++ Trial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trial
+//
+//  Created by Au Kai Hin on 20/11/2015.
+//  Copyright (c) 2015 Au Kai Hin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
